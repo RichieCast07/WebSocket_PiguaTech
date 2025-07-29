7b35c230bfa83d87c7e1bdee91d463a950856cd1
RichieCast07 <richardaton234@gmail.com>
2025-07-28 23:16:40 -0600

    add logic main

--- websocket-server/cmd/main.go
+++ websocket-server/cmd/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"WS/websocket-server/internal/app"
 	"fmt"
 	"log"
-	"WS/websocket-server/internal/app"
 )
 
 func main() {
 	server := app.NewServer()
-	addr := ":3000"
+	addr := ":8080"
 	fmt.Printf("Servidor WebSocket ejecutándose en %s\n", addr)
 	if err := server.Start(addr); err != nil {
 		log.Fatal("Error fatal en el servidor:", err)
